5739847559676df60fe7f4bd2fadb41fd8fdd50d
Fdavgreen <fdavgreen@gmail.com>
2024-07-30 16:13:40 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('c2cForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you would need to send the form data to your server or email service
+    alert('Form submitted successfully!');
+});
+
+
